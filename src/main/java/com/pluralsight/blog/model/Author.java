9a3f8a6448e28d06890cfbfcc5a98d76e6d9d6a2
Roaa Zaki <72927350+Roaa-pixel@users.noreply.github.com>
2022-08-13 01:54:37 +0200

    Update Author.java

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -16,13 +16,19 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version private Long version;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
     private String password;
+    @ManyToOne
+    private List<Post> posts;
 
     public Author() {
         super();
+        posts=new ArrayList<>();
+
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -85,10 +91,10 @@ public class Author {
     }
 
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        posts.add(post);
     }
 }
